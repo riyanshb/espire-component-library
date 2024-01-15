9ef065be7733d01ed983b8423eb7b10f80713eff
Riyansh Biswas <riyansh.biswas@espire.com>
2024-01-15 08:57:45 +0530

    Added husky, eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'eslint:recommended', 
+    'plugin:@typescript-eslint/recommended', 
+    'plugin:react-hooks/recommended', 
+    'plugin:storybook/recommended',
+
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
