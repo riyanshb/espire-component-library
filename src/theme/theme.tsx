a0ba8e5fa668673c0b987cc3a3b8c9f53621abc2
Rivanshu Jain <rivanshu.jain@espire.com>
2024-02-08 14:25:09 +0530

    themeing

---
+++ src/theme/theme.tsx
@@ -0,0 +1,74 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+      light: '#42a5f5',
+      dark: '#1565c0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#ba68c8',
+      light: '#42a5f5',
+      dark: '#7b1fa2',
+    },
+  },
+  typography: {
+    fontFamily: 'Raleway, Arial',
+    h1: {
+      fontSize: '2.5rem',
+    },
+    h2: {
+      fontSize: '2rem',
+    },
+    h3: {
+      fontSize: '1.5rem',
+    },
+    h4: {
+      fontSize: '1rem',
+    },
+    subtitle1: {
+      fontSize: 12,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      fontStyle: 'italic',
+    },
+  },
+  spacing: 8,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100,
+  },
+  transitions: {
+    duration: {
+      shortest: 150,
+      shorter: 200,
+      short: 250,
+      standard: 300,
+      complex: 375,
+      enteringScreen: 225,
+      leavingScreen: 195,
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained',
+        color: 'primary',
+      },
+    },
+  },
+});
