7f8eeab2310b9b5526f6df2dcb59d4dabbc62bcd
RivanshuJain <rivanshu.jain@espire.com>
2024-03-19 16:58:42 +0530

    message

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import VitePresetReact from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePresetReact()],
+  build: {
+    minify:false,
+    
+        outDir: 'dist', // Output directory
+    lib: {
+      entry: './src/index.ts', // Entry point for the library
+      formats: ['es', 'cjs'],
+      name: 'Appbar', // Name of the library (optional)
+      fileName: () => 'Appbar.js' // Specify the output filename
+    },
+    rollupOptions: {
+      external: /^react($|\/)/,
+      output: {
+          globals: {
+              react: 'React'
+          }
+      }
+  }
+  }
 })
